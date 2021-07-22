00935e23dd69ffe014644ee5d8f7502bb8416137
pratikshashete <59416484+pratikshashete@users.noreply.github.com>
2021-07-22 11:07:14 +0530

    Delete firstprogram.java

--- firstprogram.java
+++
@@ -1,9 +0,0 @@
-import java.applet.Applet;  
-import java.awt.Graphics;  
-public class firstprogram extends Applet{  
-  
-public void paint(Graphics g){  
-g.drawString("welcome",150,150);  
-}  
-  
-}  
